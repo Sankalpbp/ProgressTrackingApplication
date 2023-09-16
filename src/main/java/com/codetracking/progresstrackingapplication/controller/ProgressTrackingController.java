9c225c02f0724e55e2830aac69b36e3384aa34bb
Sankalpbp <sankalpnitj@gmail.com>
2023-09-16 18:38:28 +0530

    Settinp up simple method level authorization for different roles

--- src/main/java/com/codetracking/progresstrackingapplication/controller/ProgressTrackingController.java
+++ src/main/java/com/codetracking/progresstrackingapplication/controller/ProgressTrackingController.java
@@ -1,5 +1,6 @@
 package com.codetracking.progresstrackingapplication.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,13 @@ import java.util.List;
 public class ProgressTrackingController {
 
     @GetMapping ( "/welcome" )
+    @PreAuthorize ( "hasAuthority('ROLE_USER')")
     public String welcome ( ) {
         return "Hey! Welcome! You can track your progress by signing in!";
     }
 
     @GetMapping ( "/getAll" )
+    @PreAuthorize ( "hasAuthority('ROLE_ADMIN')" )
     public List<String> getAll () {
         return Arrays.asList ( "something", "the other thing" );
     }
